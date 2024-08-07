719476fb95c1a203da0653978f67b660f1a4d253
HamizMuzaffer <hamiz.muzaffer77@gmail.com>
2024-08-08 01:50:43 +0500

    image added

--- client/src/components/ViewCard.jsx
+++ client/src/components/ViewCard.jsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import { Button, Container } from '@mui/material';
 import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
+import DoNotDisturbIcon from '@mui/icons-material/DoNotDisturb';
 import { useNavigate } from 'react-router-dom';
 
 export default function ViewCard({ assignment, teacher }) {
@@ -49,21 +51,26 @@ export default function ViewCard({ assignment, teacher }) {
         title={teacher.name}
         subheader={`Due Date : ${readableDate}`}
       />
+      <CardMedia
+        component="img"
+        height="194"
+        image={assignment.file}
+        alt="Assignment image"
+      />
       <CardContent>
         <Typography variant="h4" color="text.secondary">
           {assignment.title}
         </Typography>
-        <Typography variant="body2" color="text.secondary" sx={{ mt: 1 }}>
-          {assignment.description}
-        </Typography>
-        <Button
-          variant="contained"
-          onClick={handleViewDetails}
-          color="primary"
-          sx={{ mt: 2 }}
-        >
-          View Details
-        </Button>
+        <>
+          <Button
+            variant="contained"
+            onClick={handleViewDetails}
+            color="primary"
+            sx={{ my: 1 }}
+          >
+            Submit
+          </Button>
+        </>
       </CardContent>
     </Card>
   );
